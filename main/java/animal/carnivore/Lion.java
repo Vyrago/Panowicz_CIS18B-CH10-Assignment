5aee55dcebb99e09d0e4cb19033c66be933dd70e
Vyrago <52431894+Vyrago@users.noreply.github.com>
2021-09-16 16:34:24 -0700

    Update Lion.java

--- main/java/animal/carnivore/Lion.java
+++ main/java/animal/carnivore/Lion.java
@@ -1,1 +1,23 @@
+package animal.carnivore;
+import animal.*;
 
+public class Lion implements Animal{
+
+//
+	private String name;
+	private String type = "Lion";
+
+	public Lion(String name) {
+		this.name = name;
+	}
+
+//
+	public String getName() {
+		return this.name;
+	}
+
+//
+	public String getType() {
+		return this.type;
+	}
+}

a1c3e994eedb9901d185f840a27ad1ffb3a1913b
Vyrago <52431894+Vyrago@users.noreply.github.com>
2021-09-16 16:36:27 -0700

    Update Animal.java

--- main/java/animal/Animal.java
+++ main/java/animal/Animal.java
@@ -1,1 +1,12 @@
+package animal;
+	
+public interface Animal { 
 
+	default String getType() {
+		return "This animal has no Type!";
+	}
+
+	default String getName() {
+		return "This animal has no Name!";
+	}
+}

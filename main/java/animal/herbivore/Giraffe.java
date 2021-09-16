b67d60161d54f01aba093ca660bda9e4de68b8c7
Vyrago <52431894+Vyrago@users.noreply.github.com>
2021-09-16 16:35:42 -0700

    Update Giraffe.java

--- main/java/animal/herbivore/Giraffe.java
+++ main/java/animal/herbivore/Giraffe.java
@@ -1,1 +1,23 @@
+package animal.herbivore;
+import animal.*;
 
+
+public class Giraffe implements Animal{
+
+	private String name;
+	private String type = "Giraffe";
+
+	public Giraffe(String name) {
+		this.name = name;
+	}
+
+
+	public String getName() {
+		return this.name;
+	}
+
+  
+	public String getType() {
+		return this.type;
+	}
+}
